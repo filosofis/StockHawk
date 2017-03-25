27f14124fb74be62ce3c7b5f1683dd8a2dccb9e4
filosofis <olundqvist89@gmail.com>
2017-03-25 15:55:11 +0100

    Initial commit

---
+++ app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
@@ -0,0 +1,98 @@
+package com.udacity.stockhawk.ui;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.udacity.stockhawk.R;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import yahoofinance.YahooFinance;
+
+
+public class AddStockDialog extends DialogFragment {
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.dialog_stock)
+    EditText stock;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        @SuppressLint("InflateParams") View custom = inflater.inflate(R.layout.add_stock_dialog, null);
+
+        ButterKnife.bind(this, custom);
+
+        stock.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                addStock();
+                return true;
+            }
+        });
+        builder.setView(custom);
+
+        builder.setMessage(getString(R.string.dialog_title));
+        builder.setPositiveButton(getString(R.string.dialog_add),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        addStock();
+                    }
+                });
+        builder.setNegativeButton(getString(R.string.dialog_cancel), null);
+
+        Dialog dialog = builder.create();
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+
+        return dialog;
+    }
+
+    private void addStock() {
+        String stockName = stock.getText().toString();
+        if (isAStock(stockName)) {
+            Activity parent = getActivity();
+            if (parent instanceof MainActivity) {
+                ((MainActivity) parent).addStock(stockName);
+            }
+            dismissAllowingStateLoss();
+        }
+    }
+
+    private boolean isAStock(String stock){
+        try{
+            if(YahooFinance.get(stock) != null){
+                return true;
+            }else{
+                Toast.makeText(getActivity(), "Stock not found", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }catch(IOException e){
+            Toast.makeText(getActivity(), "Connection Error", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+}
