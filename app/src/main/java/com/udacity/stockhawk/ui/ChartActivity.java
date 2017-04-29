dfafb61d2960e428db25324391f5fc0fef5f3dd1
filosofis <olundqvist89@gmail.com>
2017-04-30 01:09:37 +0200

    Implemented a loader for ChartActivity

--- app/src/main/java/com/udacity/stockhawk/ui/ChartActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/ChartActivity.java
@@ -3,7 +3,10 @@ package com.udacity.stockhawk.ui;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -22,25 +25,26 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import timber.log.Timber;
 
-public class ChartActivity extends AppCompatActivity {
+public class ChartActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    List<Entry> entries = new ArrayList<>();
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String symbol = getIntent().getStringExtra("symbol");
+        Uri getStockUri = Contract.Quote.makeUriForStock(symbol);
+        return new CursorLoader(this, getStockUri,null, null, null, null);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chart);
-        LineChart chart = (LineChart) findViewById(R.id.chart);
-        String symbol = getIntent().getStringExtra("symbol");
-        Timber.d(symbol);
-        Uri getStockUri = Contract.Quote.makeUriForStock(symbol);
-        Cursor cursor = this.getContentResolver().query(getStockUri, null, null, null, null, null);
-        cursor.moveToFirst();
+    public void onLoadFinished(Loader loader, Cursor cursor) {
 
+        cursor.moveToFirst();
+        LineChart chart = (LineChart) findViewById(R.id.chart);
         //int historyIndex = cursor.getColumnIndex(Contract.Quote.COLUMN_HISTORY);
         final String[] historyRaw = cursor.getString(Contract.Quote.POSITION_HISTORY).split("\n");
         String[] history;
-        List<Entry> entries = new ArrayList<>();
+
 
         for(int i=0; i<historyRaw.length; i++){
             //Timber.d(historyRaw[i]);
@@ -48,15 +52,15 @@ public class ChartActivity extends AppCompatActivity {
             //Timber.d(history[0] + " " + history[1]);
             entries.add(new Entry(i,Float.parseFloat(history[1])));
         }
-
-        LineDataSet dataSet = new LineDataSet(entries, "Dates");
+        //Although its no longer actually displayed
+        LineDataSet dataSet = new LineDataSet(entries, getString(R.string.data_set_label));
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.getDescription().setEnabled(false);
         chart.getLegend().setEnabled(false);
 
         chart.getAxisLeft().setTextColor(Color.WHITE);
-        chart.getAxisRight().setTextColor(Color.WHITE);
+        chart.getAxisRight().setDrawLabels(false);
         XAxis xAxis = chart.getXAxis();
         xAxis.setLabelCount(5);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -73,6 +77,23 @@ public class ChartActivity extends AppCompatActivity {
         });
         chart.invalidate();
         cursor.close();
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+        getSupportLoaderManager().initLoader(1, null, this);
+        //String symbol = getIntent().getStringExtra("symbol");
+        //Timber.d(symbol);
+        //Uri getStockUri = Contract.Quote.makeUriForStock(symbol);
+        //Cursor cursor = this.getContentResolver().query(getStockUri, null, null, null, null, null);
+
     }
 
     static String formatDate(long dateInMilliseconds) {
